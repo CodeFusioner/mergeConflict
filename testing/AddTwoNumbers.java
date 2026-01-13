f80fba719331aad601533900178c1dc5c93a470f
Kapil Gupta <kg666235@gmail.com>
2026-01-13 15:41:44 +0530

    [Kapil Gupta] Add. Added Scenario based Questions

--- testing/AddTwoNumbers.java
+++ testing/AddTwoNumbers.java
@@ -1,6 +1,6 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        int a = 500;
+        int a = 222200;
         int b = 100;
         int sum = a + b;
         System.out.println("The sum of " + a + " and " + b + " is: " + sum);
