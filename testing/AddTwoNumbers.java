7fe4446a534234e3616454e35475fdd548aee3c1
Manasverma1111 <vmanas79@gmail.com>
2026-01-13 15:39:05 +0530

    Update Prog

--- testing/AddTwoNumbers.java
+++ testing/AddTwoNumbers.java
@@ -1,7 +1,7 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
         int a = 500;
-        int b = 100;
+        int b = 900;
         int sum = a + b;
         System.out.println("The sum of " + a + " and " + b + " is: " + sum);
     }
