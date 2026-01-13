ea648d3cc1878824fd8072dd9c9eb592d5794cdb
divyansh-bhatt <divyansh.bhatt_cs22@gla.ac.in>
2026-01-13 15:49:48 +0530

    Delete testing directory

--- testing/AddTwoNumbers.java
+++
@@ -1,8 +0,0 @@
-public class AddTwoNumbers {
-    public static void main(String[] args) {
-        int a = 500;
-        int b = 900;
-        int sum = a + b;
-        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
-    }
-}
