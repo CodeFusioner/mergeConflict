c24cde69b9fae4fbf519524695ff0f79634821cf
Manasverma1111 <vmanas79@gmail.com>
2026-01-13 16:15:04 +0530

    added mult program

---
+++ testing/Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 4;
+        int mult = a * b;
+        System.out.println(mult);
+    }
+}
